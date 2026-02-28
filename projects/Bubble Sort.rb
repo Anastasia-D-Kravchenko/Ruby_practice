be3d73cd859048261e3874dce2cb9da9856eb02b
Anastasia Kravchenko <118124554+Anastasia-D-Kravchenko@users.noreply.github.com>
2026-02-28 01:55:37 +0100

    added Bubble Sort project

---
+++ projects/Bubble Sort.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  array
+end
+
+p bubble_sort([4, 3, 78, 2, 0, 2])
+# Expected output: [0, 2, 2, 3, 4, 78]
